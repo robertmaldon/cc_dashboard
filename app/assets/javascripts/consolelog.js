2818bd2964dce164153bb164afec90de47bf8d49
robertmaldon <robertmaldon@gmail.com>
2013-01-14 21:26:23 +1100

    Create a dummy implementation of console.log on browsers that do not implement console

--- app/assets/javascripts/consolelog.js
+++ app/assets/javascripts/consolelog.js
@@ -1,8 +1,8 @@
 // For those browsers that do not support console.log (yep, IE8 or earlier)
 // define console.log as an empty function so that modern-ish javascript
-// libs that expect console.log do not break. Code snippet coppied from
+// libs that expect console.log do not break. Code snippet adapted from
 // http://blog.patspam.com/2009/the-curse-of-consolelog
-if (!("console" in window) || !("firebug" in console)) {
+if (typeof(console) === "undefined") {
   var names = ["log", "debug", "info", "warn", "error", "assert", "dir", "dirxml", "group", "groupEnd", "time", "timeEnd", "count", "trace", "profile", "profileEnd"];
   window.console = {};
   for (var i = 0, len = names.length; i < len; ++i) {
