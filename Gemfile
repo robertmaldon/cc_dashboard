d545f886fb3444942fa8a9ba4fc6d0cb6d0ac0c2
robertmaldon <robertmaldon@gmail.com>
2014-01-19 21:55:05 +1100

    Small changes to support installing and running on osx mavericks

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ group :assets do
   gem 'coffee-rails', '~> 3.2.1'
 
   # See https://github.com/sstephenson/execjs#readme for more supported runtimes
-  gem 'therubyracer'
+  gem 'therubyracer', '~> 0.12.0'
 
   gem 'uglifier', '>= 1.0.3'
 end
--- stop.sh
+++ stop.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # A sample UNIX script that can be used to stop cc_dashboard from
 # init.d/cron
-CC_DASHBOARD_PID=`/usr/bin/lsof -i TCP:3332|grep -v PID|awk '{print $2}'`
+if [ -f /usr/bin/lsof ]
+then
+    LSOF=/usr/bin/lsof
+elif [ -f /usr/sbin/lsof ]
+then
+    LSOF=/usr/sbin/lsof
+else
+	echo "Could not find location of lsof"
+	exit 1
+fi
+
+CC_DASHBOARD_PID=`$LSOF -i TCP:3332|grep -v PID|awk '{print $2}'`
 if [ "x$CC_DASHBOARD_PID" != "x" ]; then
     echo "Killing cc_dashboard PID $CC_DASHBOARD_PID"
     kill $CC_DASHBOARD_PID
